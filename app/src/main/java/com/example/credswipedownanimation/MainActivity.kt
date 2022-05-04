cbe09fe8f65572c4dc8ab45f3b0f16f5e7d7bf44
Madhur Gupta <madhurgupta10@gmail.com>
2022-05-05 02:40:09 +0530

    Add basic animation

--- app/src/main/java/com/example/credswipedownanimation/MainActivity.kt
+++ app/src/main/java/com/example/credswipedownanimation/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.credswipedownanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import com.example.credswipedownanimation.ui.theme.CredSwipedownAnimationTheme
+import kotlin.math.roundToInt
 
+@ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +29,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val height = 300.dp
+                    val squareSize = 100.dp
+
+                    val swipeableState = rememberSwipeableState(0)
+                    val sizePx = with(LocalDensity.current) { height.toPx() }
+                    val anchors =
+                        mapOf(0f to 0, sizePx to 1) // Maps anchor points (in px) to states
+
+                    Box(
+                        modifier = Modifier
+                            .padding(100.dp)
+                            .swipeable(
+                                enabled = swipeableState.offset.value.roundToInt() != 1,
+                                state = swipeableState,
+                                anchors = anchors,
+                                velocityThreshold = Dp.Infinity,
+                                thresholds = { _, _ -> FractionalThreshold(1f) },
+                                orientation = Orientation.Vertical
+                            )
+                            .background(Color.LightGray)
+                    ) {
+                        Box(
+                            Modifier
+                                .offset { IntOffset(0, swipeableState.offset.value.roundToInt()) }
+                                .size(squareSize)
+                                .background(Color.DarkGray)
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CredSwipedownAnimationTheme {
-        Greeting("Android")
     }
 }
