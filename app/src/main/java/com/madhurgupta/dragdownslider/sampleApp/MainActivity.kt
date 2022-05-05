e074f3b62e920d8c3d5db404857343c4beb7276e
Madhur Gupta <madhurgupta10@gmail.com>
2022-05-05 23:18:37 +0530

    Update UI and add onDispose

--- app/src/main/java/com/madhurgupta/dragdownslider/sampleApp/MainActivity.kt
+++ app/src/main/java/com/madhurgupta/dragdownslider/sampleApp/MainActivity.kt
@@ -3,9 +3,13 @@ package com.madhurgupta.dragdownslider.sampleApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.madhurgupta.dragdownslider.DragDownSlider
 import com.madhurgupta.dragdownslider.sampleApp.ui.theme.SampleAppForDragDownSlider
 
@@ -17,8 +21,8 @@ class MainActivity : ComponentActivity() {
             SampleAppForDragDownSlider {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+                    color = Color.DarkGray
                 ) {
                     DragDownSlider()
                 }
--- dragdownslider/src/main/java/com/madhurgupta/dragdownslider/DragDownSlider.kt
+++ dragdownslider/src/main/java/com/madhurgupta/dragdownslider/DragDownSlider.kt
@@ -1,53 +1,126 @@
 package com.madhurgupta.dragdownslider
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FractionalThreshold
-import androidx.compose.material.rememberSwipeableState
-import androidx.compose.material.swipeable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
 @ExperimentalMaterialApi
 @Composable
 fun DragDownSlider() {
-    val height = 300.dp
-    val squareSize = 100.dp
 
-    val swipeableState = rememberSwipeableState(0)
-    val sizePx = with(LocalDensity.current) { height.toPx() }
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+
+    val cardSize = screenWidth
+    val circleSize = 100.dp
+    val distanceBetweenCircles = 300.dp
+
+    val startPosition = 0
+    val endPosition = 1
+
+    val isDragEnabled = rememberSaveable { mutableStateOf(true) }
+    val swipeableState = rememberSwipeableState(startPosition)
+    val distanceBetweenCirclesInPx =
+        with(LocalDensity.current) { (distanceBetweenCircles - circleSize).toPx() }
     val anchors =
-        mapOf(0f to 0, sizePx to 1) // Maps anchor points (in px) to states
+        mapOf(0f to startPosition, distanceBetweenCirclesInPx to endPosition) // Maps anchor points (in px) to states
 
-    Box(
+    val scope = rememberCoroutineScope()
+    if (swipeableState.isAnimationRunning) {
+        DisposableEffect(Unit) {
+            onDispose {
+//                isDragEnabled.value = false
+                when (swipeableState.currentValue) {
+                    endPosition -> {
+                        scope.launch {
+                            swipeableState.snapTo(startPosition)
+                        }
+                    }
+                }
+            }
+        }
+    }
+//    if (swipeableState.currentValue == endPosition) {
+//        DisposableEffect(Unit) {
+//            onDispose {
+//                isDragEnabled.value = false
+//            }
+//        }
+//    }
+
+    Column(
         modifier = Modifier
-            .padding(100.dp)
-            .swipeable(
-                enabled = swipeableState.offset.value.roundToInt() != 1,
-                state = swipeableState,
-                anchors = anchors,
-                velocityThreshold = Dp.Infinity,
-                thresholds = { _, _ -> FractionalThreshold(1f) },
-                orientation = Orientation.Vertical
-            )
-            .background(Color.LightGray)
+            .height(cardSize + distanceBetweenCircles + circleSize)
+            .padding(24.dp),
     ) {
-        Box(
-            Modifier
-                .offset { IntOffset(0, swipeableState.offset.value.roundToInt()) }
-                .size(squareSize)
-                .background(Color.DarkGray)
-        )
+        Box {
+            Card(
+                shape = RoundedCornerShape(8.dp),
+                backgroundColor = Color.White,
+                modifier = Modifier.size(cardSize)
+            ) {}
+            Column(
+                modifier = Modifier
+                    .height(distanceBetweenCircles)
+                    .align(Alignment.BottomCenter)
+                    .offset(y = distanceBetweenCircles - circleSize / 2)
+                    .swipeable(
+                        enabled = isDragEnabled.value,
+                        state = swipeableState,
+                        anchors = anchors,
+                        velocityThreshold = Dp.Infinity,
+                        thresholds = { _, _ -> FractionalThreshold(1f) },
+                        orientation = Orientation.Vertical,
+                    )
+                    .background(Color.Transparent),
+                verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Card(
+                    shape = CircleShape,
+                    elevation = 4.dp,
+                    backgroundColor = Color.Black,
+                    modifier = Modifier
+                        .width(circleSize)
+                        .aspectRatio(1f)
+                        .offset {
+                            IntOffset(0, swipeableState.offset.value.roundToInt())
+                        }
+                        .clip(CircleShape)
+                        .zIndex(1f)
+                ) {}
+
+                Card(
+                    shape = CircleShape,
+                    elevation = 0.dp,
+                    contentColor = Color.Transparent,
+                    backgroundColor = Color.Transparent,
+                    border = BorderStroke(1.dp, Color.LightGray),
+                    modifier = Modifier
+                        .size(circleSize)
+                        .clip(CircleShape)
+                ) {}
+            }
+        }
     }
 }
